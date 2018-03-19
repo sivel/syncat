2490df87873195530fa3f16a6540966862286609
Matt Martz <matt@sivel.net>
2018-03-19 10:25:33 -0500

    Bump to 1.0.1

--- syncat/__init__.py
+++ syncat/__init__.py
@@ -30,7 +30,7 @@ from pygments.util import ClassNotFound
 from .solarized256 import Solarized256Style
 
 
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 
 
 class _HelpFormatter(argparse.HelpFormatter):
